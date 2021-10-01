12778ef930f38d458f1674f1eafee207c7566c9a
puramjyothirmai1 <jyothyrmai.puram@gmail.com>
2021-10-01 17:07:31 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -15,7 +15,8 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React from jyothirmai
+          if u dont want jfo
         </a>
       </header>
     </div>
